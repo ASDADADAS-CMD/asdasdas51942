c2f0f6ed12c10501eef6e9116659d33635017c19
ASDADADAS-CMD <63318958+ASDADADAS-CMD@users.noreply.github.com>
2020-11-23 19:36:16 +0300

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "yardım",
+  aliases: ["h"],
+  description: "Komutların adını ve açıklamalarını gösterir.",
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+      .setTitle(`${message.client.user.username} Help`)
+      .setDescription("Tüm Komutları Listeler")
+      .setColor("#F8AA2A");
+
+    commands.forEach((cmd) => {
+      helpEmbed.addField(
+        `**${message.client.prefix}${cmd.name} ${cmd.aliases ? `(${cmd.aliases})` : ""}**`,
+        `${cmd.description}`,
+        true
+      );
+    });
+
+    helpEmbed.setTimestamp();
+
+    return message.channel.send(helpEmbed).catch(console.error);
+  }
+};
---
+++ commands/uptime.js
@@ -0,0 +1,19 @@
+module.exports = {
+  name: "sureyi-goster",
+  aliases: ["u"],
+  description: "Çalan müziğin süresini gösterir.",
+  execute(message) {
+    let seconds = Math.floor(message.client.uptime / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+    let days = Math.floor(hours / 24);
+
+    seconds %= 60;
+    minutes %= 60;
+    hours %= 24;
+
+    return message
+      .reply(`Uptime: \`${days} day(s),${hours} hours, ${minutes} minutes, ${seconds} seconds\``)
+      .catch(console.error);
+  }
+};
